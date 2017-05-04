232784d0777eb5ef280e584564d8cacc4ac25c0f
iontiger <iontiger@iontiger-iMac.local>
2017-05-04 21:18:41 +0900

    [part3] angularfire2 setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AngularFireModule } from 'angularfire2';
+import { FirebaseService } from './services/firebase.service';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -11,6 +13,15 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { AddlistingComponent } from './components/addlisting/addlisting.component';
 import { EditlistingComponent } from './components/editlisting/editlisting.component';
 import { ListingComponent } from './components/listing/listing.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBCtP6io3YvqIrfjOgXmFxN8YfUjxxVxzE",
+    authDomain: "kadap-1ec3a.firebaseapp.com",
+    databaseURL: "https://kadap-1ec3a.firebaseio.com",
+    projectId: "kadap-1ec3a",
+    storageBucket: "kadap-1ec3a.appspot.com",
+    messagingSenderId: "279721323291"  
+};
 
 const appRoutes: Routes = [
   {path: '', component:HomeComponent},
@@ -32,9 +43,10 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [FirebaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
