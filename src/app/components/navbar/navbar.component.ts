d2e2b04557dc269fb074a2557a9ddd370052c740
iontiger <iontiger@iontiger-iMac.local>
2017-05-11 00:55:32 +0900

    redirect home when logout

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFire } from 'angularfire2';
 import { FlashMessagesService } from 'angular2-flash-messages';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -11,7 +12,8 @@ export class NavbarComponent implements OnInit {
 
   constructor(
     public af:AngularFire,
-    public flashMessage:FlashMessagesService
+    public flashMessage:FlashMessagesService,
+    private router:Router
     ) { }
 
   ngOnInit() {
@@ -24,5 +26,6 @@ export class NavbarComponent implements OnInit {
   logout(){
     this.af.auth.logout();
     this.flashMessage.show('안전하게 로그아웃되었습니다', {cssClass: 'alert-success', timeout: 3000});
+    this.router.navigate(['/']);
   }
 }
